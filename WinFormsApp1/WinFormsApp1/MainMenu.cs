8692ab21102206644fa3e133ee2a216fcb0d29d5
bishop-casey <112920878+bishop-casey@users.noreply.github.com>
2022-11-18 01:35:10 -0500

    Update MainMenu.cs

--- WinFormsApp1/WinFormsApp1/MainMenu.cs
+++ WinFormsApp1/WinFormsApp1/MainMenu.cs
@@ -40,25 +40,3 @@ namespace WinFormsApp1
         
     }
 }
-=======
-﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
-using System.Drawing;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.Windows.Forms;
-
-namespace WinFormsApp1
-{
-    public partial class MainMenu : Form
-    {
-        public MainMenu()
-        {
-            InitializeComponent();
-        }
-    }
-}
->>>>>>> 8afa7d4ddaa720656a251fed05001709bb3ba9e2
